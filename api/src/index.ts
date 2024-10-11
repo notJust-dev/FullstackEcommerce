8fbd6052173eab571b3d42b8a67021f48f7f644a
Vadim Savin <savinvadim1312@gmail.com>
2024-10-11 16:10:54 +0200

    Setup api and express

---
+++ api/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
