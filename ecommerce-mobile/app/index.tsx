1a02a0cdfab3c2a2f139540a50c174b2e81848e2
Vadim Savin <savinvadim1312@gmail.com>
2024-10-18 16:46:51 +0200

    Product Item UI using Gluestack

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,18 +1,15 @@
-import { View, Text, FlatList } from 'react-native';
+import { FlatList } from 'react-native';
 import products from '../assets/products.json';
 import ProductListItem from '../components/ProductListItem';
-import { Button, ButtonText } from '@/components/ui/button';
 
 export default function HomeScreen() {
   return (
-    <Button variant="outline">
-      <ButtonText>Press me</ButtonText>
-    </Button>
+    <FlatList
+      data={products}
+      numColumns={2}
+      contentContainerClassName="gap-2"
+      columnWrapperClassName="gap-2"
+      renderItem={({ item }) => <ProductListItem product={item} />}
+    />
   );
-  // return (
-  //   <FlatList
-  //     data={products}
-  //     renderItem={({ item }) => <ProductListItem product={item} />}
-  //   />
-  // );
 }
