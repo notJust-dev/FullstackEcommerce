6f47ff2b2650eca3ad463a64d3bd26603061702f
Vadim Savin <savinvadim1312@gmail.com>
2024-10-18 16:03:15 +0200

    setup expo router

---
+++ ecommerce-mobile/app/cart.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function CartScreen() {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Cart screen</Text>
+    </View>
+  );
+}
---
+++ ecommerce-mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Home screen</Text>
+    </View>
+  );
+}
