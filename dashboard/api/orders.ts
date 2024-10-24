7734d06b0d7aa3b8200e09ecbc76d7aa3930d2fd
Vadim Savin <savinvadim1312@gmail.com>
2024-10-24 18:41:22 +0200

    orders list

---
+++ dashboard/api/orders.ts
@@ -0,0 +1,25 @@
+import { API_URL } from '@/config';
+import { cookies } from 'next/headers';
+
+export async function fetchOrders() {
+  try {
+    const token = cookies().get('token')?.value;
+
+    const response = await fetch(`${API_URL}/orders`, {
+      headers: {
+        Authorization: token ?? '',
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      console.error(response);
+      throw new Error('Failed to fetch orders');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
---
+++ dashboard/app/dashboard/orders/page.tsx
@@ -0,0 +1,28 @@
+import { fetchOrders } from '@/api/orders';
+import { Box } from '@/components/ui/box';
+import { Card } from '@/components/ui/card';
+import { HStack } from '@/components/ui/hstack';
+import { Text } from '@/components/ui/text';
+import dayjs from 'dayjs';
+
+export default async function OrdersPage() {
+  const orders = await fetchOrders();
+
+  return (
+    <Card className="w-full max-w-screen-lg">
+      <HStack className="p-4 border-b border-gray-200 gap-4">
+        <Text className="font-bold">Id</Text>
+        <Text className="font-bold">Date</Text>
+        <Text className="ml-auto font-bold">Status</Text>
+      </HStack>
+
+      {orders.map((order) => (
+        <HStack key={order.id} className="p-4 border-b border-gray-200 gap-4">
+          <Text>{order.id}</Text>
+          <Text>{dayjs(order.createdAt).format('DD/MM/YYYY HH:mm')}</Text>
+          <Text className="ml-auto">{order.status}</Text>
+        </HStack>
+      ))}
+    </Card>
+  );
+}
